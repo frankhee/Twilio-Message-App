f357bcc9b42a1a2225606c4856ba60f0f5aceac8
Yangrongrui He <frankhe@Yangrongruis-MacBook-Pro.local>
2020-09-12 21:10:10 -0400

    Set up client side and connect it to the server.

---
+++ client/src/App.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+  Typography, 
+  Input,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  IconButton 
+} from '@material-ui/core';
+import { SendOutlined } from '@material-ui/icons';
+
+const useStyles = makeStyles(() => ({
+  pageContainer: {
+    width: '100vw',
+    height: '100vh',
+  },
+  topContainer: {
+    width: '100%',
+    height: '60%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    width: '100%',
+    height: '40%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  searchBar: {
+    width: '50%',
+    alignSelf: 'center'
+  }
+}));
+function App() {
+  const classes = useStyles();
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSubmit() {
+    console.log("Handle submit ran")
+    const requestBody = { 
+      message: inputValue
+    };
+    axios.post(`http://localhost:7000/text`, { requestBody })
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+  };
+
+  return (
+    <div className={classes.pageContainer}>
+      <div className={classes.topContainer}>
+        <Typography 
+          variant="h1" 
+          component="h2" 
+          style={{
+            alignSelf: 'center',
+            marginBottom: '50px',
+            textAlign: 'center'
+          }}
+        >
+          Welcome to my Twilio App
+        </Typography>
+        <Typography 
+          variant="h4" 
+          style={{
+            alignSelf: 'center',
+            wordWrap: 'break-word',
+            textAlign: 'center'
+          }}
+        >
+          Hi there! To learn more about me, submit your phone number below and receive a message with more information. 
+        </Typography>
+      </div>
+      <div className={classes.bottomContainer}>
+        <FormControl className={classes.searchBar}>
+          <InputLabel htmlFor="standard-adornment-search">Your phone number(US Number only)</InputLabel>
+          <Input
+            id="submit-bar"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="submit-search"
+                  onClick={() => handleSubmit()}
+                >
+                  <SendOutlined/>
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const accountSid = process.env.REACT_APP_SID;
+const authToken = process.env.REACT_APP_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.use(cors());
+app.use(express.json());
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
@@ -11,6 +18,29 @@ app.post('/sms', (req, res) => {
   res.end(twiml.toString());
 });
 
+app.post('/text', (req, res) => {
+  try {
+    req.setTimeout(0);
+    const options = {
+      from: '+18043699101',
+      to: `+1${req.body.requestBody.message}`,
+      body: 'Hello World'
+    }
+    sendMessage(options);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+function sendMessage(options) {
+  client.messages.create(options, function(err, response) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log("Message sent successfully!")
+    }
+  })
+};
 
 app.listen(7000, function () {
   console.log('Server has started on port 7000!');
